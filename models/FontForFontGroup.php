8344a074b8b8b3997f7ecc9d9a25c8da991afe79
Ferdous Islam <ferdouszislam@gmail.com>
2022-11-13 00:41:15 +0600

    added FontGroup, FontForFontGroup models

---
+++ models/FontForFontGroup.php
@@ -0,0 +1,54 @@
+<?php
+
+class FontForFontGroup extends BaseModel {
+
+    protected $table = "font_for_font_group";
+
+    public $id;
+    public $fontName;
+    public $fontGroupId;
+    public $selectedFontId;
+    public $specificSize;
+    public $priceChange;
+
+    public function create() {
+        $query = 'INSERT INTO ' . $this->table . ' SET font_name = :fontName, font_group_id = :fontGroupId, '
+        . 'font_id = :selectedFontId, specific_size = :specificSize, price_change = :priceChange';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':fontName', $this->fontName);
+        $stmt->bindParam(':fontGroupId', $this->fontGroupId);
+        $stmt->bindParam(':selectedFontId', $this->selectedFontId);
+        $stmt->bindParam(':specificSize', $this->specificSize);
+        $stmt->bindParam(':priceChange', $this->priceChange);
+        if ($stmt->execute()) return true;
+        printf("error occurred in FontForFontGroup.create(): %s.\n", $stmt->error);
+        return false;
+    }
+
+    public function readByFontGroupId($fontGroupId) {
+        $query = 'SELECT t.id, t.font_name, t.font_group_id, t.font_id, t.specific_size, t.price_change' 
+        . ' FROM ' . $this->table . ' t WHERE t.font_group_id = ? LIMIT 0,1';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $fontGroupId);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (gettype($result) == 'boolean') return null;
+        $this->id = $result['id'];
+        $this->fontName = $result['font_name'];
+        $this->fontGroupId = $result['font_group_id'];
+        $this->selectedFontId = $result['font_id'];
+        $this->specificSize = $result['specific_size'];
+        $this->priceChange = $result['price_change'];
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return '[id: ' . $this->id . " fontName: " . $this->fontName 
+        . " fontGroupId: " . $this->fontGroupId . " selectedFontId: " . $this->selectedFontId 
+        . " specificSize: " . $this->specificSize . " priceChange: " . $this->priceChange . ']';
+    }
+
+}
+
+?>
---
+++ models/FontGroup.php
@@ -0,0 +1,95 @@
+<?php
+
+include_once 'BaseModel.php';
+
+class FontGroup extends BaseModel {
+
+    protected $table = "font_group";
+
+    public $id;
+    public $fontGroupName;
+    public $selectedFontList = array(); // array of FontForFontGroup.php model objects
+
+    public function create()
+    {
+        $query = 'INSERT INTO ' . $this->table . ' SET font_group_name = :fontGroupName';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':fontGroupName', $this->fontGroupName);
+        if (!$stmt->execute()) {
+            printf("error occurred in FontGroup.create(): %s.\n", $stmt->error);
+            return false;
+        }
+        $this->id = $this->conn->lastInsertId();
+        foreach($this->selectedFontList as $fontForFontGroup) {
+            $fontForFontGroup->fontGroupId = $this->id;
+            if (!$fontForFontGroup->create()) {
+                printf("failed to create fontForFontGroup: %s in FontGroup.create()\n", strval($fontForFontGroup));
+            }
+        }
+        return true;
+    }
+
+    public function readAll() 
+    {
+        $fontGroups = array();
+        $query = 'SELECT fg.id as fg_id, fg.font_group_name,'
+        .' ffg.id as ffg_id, ffg.font_name, ffg.font_id, ffg.specific_size, ffg.price_change' 
+        . ' FROM ' . $this->table . ' fg' 
+        . ' LEFT JOIN font_for_font_group ffg on fg.id = ffg.font_group_id';
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        $result = $stmt;
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            $found = false;
+            for($i = 0; $i < count($fontGroups); $i++) {
+                if ($fontGroups[$i]->id == $row['fg_id']) {
+                    $found = true;
+                    $fontGroups[$i] = $this->add_font_for_group($fontGroups[$i], $row);
+                } 
+            }
+            if (!$found) {
+                $fontGroups[$i] = new FontGroup();
+                $fontGroups[$i]->id = $row['fg_id'];
+                $fontGroups[$i]->fontGroupName = $row['font_group_name'];
+                $fontGroups[$i] = $this->add_font_for_group($fontGroups[$i], $row);
+            }
+        }
+        return  $fontGroups;
+    }
+
+    public function read($id) 
+    {
+        $query = 'SELECT fg.id as fg_id, fg.font_group_name,'
+        .' ffg.id as ffg_id, ffg.font_name, ffg.font_id, ffg.specific_size, ffg.price_change' 
+        . ' FROM ' . $this->table . ' fg' 
+        . ' LEFT JOIN font_for_font_group ffg on fg.id = ffg.font_group_id'
+        . ' WHERE fg.id = ?';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $id);
+        $stmt->execute();
+        $result = $stmt;
+        $found = false;
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            $found = true;
+            $this->id = $row['fg_id'];
+            $this->fontGroupName = $row['font_group_name'];
+            $this->add_font_for_group($this, $row);
+        }
+        return $found ? $this : null;
+    }
+
+    private function add_font_for_group($fontGroup, $row) {
+        $fontForFontGroup = new FontForFontGroup();
+        $fontForFontGroup->id = $row['ffg_id'];
+        $fontForFontGroup->fontGroupId = $row['fg_id'];
+        $fontForFontGroup->fontName = $row['font_name'];
+        $fontForFontGroup->selectedFontId = $row['font_id'];
+        $fontForFontGroup->specificSize = $row['specific_size'];
+        $fontForFontGroup->priceChange = $row['price_change'];
+        array_push($fontGroup->selectedFontList, $fontForFontGroup);
+        return $fontGroup;
+    }
+
+}
+
+?>
