184b514417886c8ee3d91b188f6f7ee005ac70f7
Ferdous Islam <ferdouszislam@gmail.com>
2022-11-12 07:17:58 +0600

    added create font by uploading file

--- api/font.php
+++ api/font.php
@@ -4,7 +4,8 @@ include_once '../util/init.php';
 include_once '../models/Font.php';
 
 $REQUEST_METHOD = $_SERVER['REQUEST_METHOD'];
-$UPLOAD_DIR = '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Assets' . DIRECTORY_SEPARATOR;
+$UPLOAD_DIR = 'src' . DIRECTORY_SEPARATOR . 'Assets' . DIRECTORY_SEPARATOR;
+$RELATIVE_UPLOAD_DIR = '..' . DIRECTORY_SEPARATOR . $UPLOAD_DIR;
 $FONT_FILE = 'fontFile';
 
 $URL_PARAMS = array();
@@ -16,7 +17,7 @@ $response = array();
 switch($REQUEST_METHOD) {
 
     case "POST":
-        $response = createFont($FONT_FILE, $UPLOAD_DIR, $db);
+        $response = createFont($FONT_FILE, $UPLOAD_DIR, $RELATIVE_UPLOAD_DIR, $db);
         break;
 
     case "GET":
@@ -42,59 +43,71 @@ switch($REQUEST_METHOD) {
 
 echo json_encode($response);
 
-function createFont($FONT_FILE, $UPLOAD_DIR, $db) {
-    if ($_FILES[$FONT_FILE]) {
-        $file_name =  $_FILES[$FONT_FILE]["name"];
-        $file_tmp_name = $_FILES[$FONT_FILE]["tmp_name"];
-        $file_size = $_FILES[$FONT_FILE]["size"] / 1000.00 . "KB";
-        $error = $_FILES[$FONT_FILE]["error"];
-    
-        if ($error > 0) {
-            return array(
-                "status" => "error",
-                "error" => true,
-                "message" => "Error uploading the file: " . $error
-            );
-        } else {
-            $upload_path = $UPLOAD_DIR . $file_name;
-            $upload_path = preg_replace('/\s+/', '-', $upload_path);
-            try {
-                move_uploaded_file($file_tmp_name, $upload_path);
-            } catch (Exception $e) {
-                return array(
-                    "status" => "error",
-                    "error" => true,
-                    "message" => "Error uploading the file: " . $e->getMessage()
-                );
-            }
-    
-            // TODO: add font to db
-
-            // $host = "localhost";
-            // $user = "root";
-            // $password = "";
-            // $dbname = "testingdb";
-
-            // $con = mysqli_connect($host, $user, $password, $dbname);
-
-            // if (!$con) {
-            //     die("Connection failed: " . mysqli_connect_error());
-            // }
-
-            // $sql = "insert into users (username, name, photo) values ('cairocoders', 'cairocoders Ednalan', '$upload_path')";
-            // mysqli_query($con, $sql);
-
-            return array(
-                "status" => "success",
-                "error" => false,
-                "message" => "file uploaded successfully. [path: '" . $upload_path . "', size: " . $file_size . " KB]"
-            );
-        }
-    } else {
+function createFont($FONT_FILE, $UPLOAD_DIR, $RELATIVE_UPLOAD_DIR, $db) {
+    if (!$_FILES[$FONT_FILE]) {
         return array(
             "status" => "error",
             "error" => true,
             "message" => "No file provided"
+        );
+    }
+    try {
+        if (!is_dir($RELATIVE_UPLOAD_DIR)) mkdir($RELATIVE_UPLOAD_DIR, 077, true);
+        $file_name = $_FILES[$FONT_FILE]["name"];
+        $file_tmp_name = $_FILES[$FONT_FILE]["tmp_name"];
+        $file_size = $_FILES[$FONT_FILE]["size"] / 1000.00 . "KB";
+        $upload_path = $UPLOAD_DIR . $file_name;
+        $upload_path = preg_replace('/\s+/', '-', $upload_path);
+        $upload_relative_path = $RELATIVE_UPLOAD_DIR . $file_name;
+        $upload_relative_path = preg_replace('/\s+/', '-', $upload_relative_path);
+        $error = $_FILES[$FONT_FILE]["error"];
+        if ($error > 0) {
+            return array(
+                "status" => "error",
+                "error" => true,
+                "message" => $error == 4 ? "no file attached" : "error uploading the file",
+                "data" => array(
+                    "errorCode" => $error
+                )
+            );
+        }
+        if (file_exists($upload_relative_path)) {
+            return array(
+                "status" => "error",
+                "error" => false,
+                "message" => "font file already exists"
+            );
+        }
+        try {
+            move_uploaded_file($file_tmp_name, $upload_relative_path);
+        } catch (Exception $e) {
+            return array(
+                "status" => "error",
+                "error" => true,
+                "message" => "error uploading the file: " . $e->getMessage()
+            );
+        }
+        $font = new Font($db);
+        $font->font_name = $file_name;
+        $font->file_path = $upload_path;
+        $font->file_size = $file_size;
+        $success = $font->create();
+        if (!$success) {
+            throw new Exception("failed to save font in db");
+        }
+        return array(
+            "status" => "success",
+            "error" => false,
+            "message" => "font created successfully"
+        );
+    } catch (Exception $e) {
+        // delete uploaded font file if uploaded
+        if (file_exists($upload_relative_path)) unlink($upload_relative_path);
+        return array(
+            "status" => "error",
+            "error" => true,
+            "message" => "failed to create font: " . $e->getMessage(),
+            "data" => $e
         );
     }
 }
--- models/Font.php
+++ models/Font.php
@@ -7,20 +7,29 @@ class Font extends BaseModel
 
     public $table = 'font';
 
+    const FILE_SIZE_UNIT = 'KB';
+
     public $id;
     public $font_name;
     public $file_path;
     public $file_size;
-    public $created_at;
 
-    public function create() {
-        
+    public function create()
+    {
+        $query = 'INSERT INTO ' . $this->table . ' SET font_name = :font_name, file_path = :file_path, file_size = :file_size';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':font_name', $this->font_name);
+        $stmt->bindParam(':file_path', $this->file_path);
+        $stmt->bindParam(':file_size', $this->file_size);
+        if ($stmt->execute()) return true;
+        printf("error occurred in Font.create(): %s.\n", $stmt->error);
+        return false;
     }
 
     public function readAll()
     {
         $fonts = array();
-        $query = 'SELECT t.id, t.font_name, t.file_path, t.file_size, t.created_at FROM ' . $this->table . ' t';
+        $query = 'SELECT t.id, t.font_name, t.file_path, t.file_size FROM ' . $this->table . ' t';
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
         $result = $stmt;
@@ -35,7 +44,7 @@ class Font extends BaseModel
         return  $fonts;
     }
 
-    public function delete() {
-
+    public function delete()
+    {
     }
 }
